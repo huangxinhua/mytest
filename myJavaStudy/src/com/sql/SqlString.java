e18efe7bbfc65436b20d8ea37e15bd2ef3b92db8
huangxinhua <dream.garden@163.com>
2017-06-21 22:27:39 +0800

    update SqlString.java
    test

--- myJavaStudy/src/com/sql/SqlString.java
+++ myJavaStudy/src/com/sql/SqlString.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-
+//update
 public class SqlString {
 
 	public static void main(String args[])throws IOException{
